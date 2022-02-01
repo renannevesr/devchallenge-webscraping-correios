8ecd70c26dedb412af5c46b4e4fd5bb91a5d6ca5
Renan Rego <80763880+renannevesr@users.noreply.github.com>
2022-02-01 20:40:16 -0300

    implementação da GUI e ocultação do navegador

--- correios.py
+++ correios.py
@@ -1,28 +1,54 @@
 from selenium import webdriver
-from webdriver_manager.chrome import *
-import time
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from PySimpleGUI import PySimpleGUI as sg
 
-cep = input(' Insira o cep:')
-browser = webdriver.Chrome(ChromeDriverManager().install())
-url = 'https://buscacepinter.correios.com.br/app/endereco/index.php'
-browser.get(url)
-time.sleep(2)
-Acessar = browser.find_element_by_xpath(
-    '//*[@id="endereco"]')
-time.sleep(2)
-Acessar.send_keys(cep)
-time.sleep(2)
-pesquisa = browser.find_element_by_xpath(
-    '//*[@id="btn_pesquisar"]')
-pesquisa.click()
-time.sleep(2)
-try:
-    logradouro = browser.find_element_by_xpath(
-        '//*[@id="resultado-DNEC"]/tbody/tr/td[1]').text
-    bairro = browser.find_element_by_xpath(
-        '//*[@id="resultado-DNEC"]/tbody/tr/td[2]').text
-    localidade = browser.find_element_by_xpath(
-        '//*[@id="resultado-DNEC"]/tbody/tr/td[3]').text
-    print(logradouro, bairro, localidade)
-except:
-    print('CEP não encontrado')
+option = Options()
+option.headless = True
+
+
+def pesquisa():
+    browser = webdriver.Firefox(options=option)
+    url = 'https://buscacepinter.correios.com.br/app/endereco/index.php'
+    browser.get(url)
+    browser.implicitly_wait(1)
+    Acessar = browser.find_element(By.XPATH, '//*[@id="endereco"]')
+    Acessar.send_keys(cep)
+    pesquisa = browser.find_element(By.XPATH,
+                                    '//*[@id="btn_pesquisar"]')
+    pesquisa.click()
+    browser.implicitly_wait(2)
+
+    try:
+        logradouro = browser.find_element(By.XPATH,
+                                          '//*[@id="resultado-DNEC"]/tbody/tr/td[1]').text
+        bairro = browser.find_element(By.XPATH,
+                                      '//*[@id="resultado-DNEC"]/tbody/tr/td[2]').text
+        localidade = browser.find_element(By.XPATH,
+                                          '//*[@id="resultado-DNEC"]/tbody/tr/td[3]').text
+        janela.FindElement('_output_').Update('')
+        print("Logradouro: " + logradouro)
+        print("Bairro: " + bairro)
+        print("Cidade: " + localidade)
+    except:
+        janela.FindElement('_output_').Update('')
+        print('CEP não encontrado')
+
+
+sg.theme('LightGrey2')
+layout2 = [
+    [sg.Text('Cep'), sg.Input(key='CepPesquisa')],
+    [sg.Output(size=(40, 10), key='_output_')],
+    [sg.Button('Pesquisar'), sg.Button('Limpar')]
+]
+janela = sg.Window('Busca de cep', layout2)
+while True:
+    eventos, valores = janela.read() 
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Pesquisar':
+        cep = valores['CepPesquisa']
+        print("Aguarde estamos localizando o CEP")
+        pesquisa()
+    if eventos == 'Limpar':
+        janela.FindElement('_output_').Update('')
